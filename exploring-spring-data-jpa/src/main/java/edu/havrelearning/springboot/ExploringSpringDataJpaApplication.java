f4c5f9f8a81b748940512a967346b90740229029
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-11 05:16:22 +0700

    1.3.1 Additional

---
+++ exploring-spring-data-jpa/src/main/java/edu/havrelearning/springboot/ExploringSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package edu.havrelearning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringSpringDataJpaApplication.class, args);
+	}
+
+}
