03e9ba93f878cc1189ceb3e85d9af316c2e5b558
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-10 16:05:48 +0700

    1.1.0 Hello Spring Boot Rest

---
+++ hello-spring-boot-rest/src/test/java/edu/havrelearning/springboot/HelloSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.havrelearning.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
