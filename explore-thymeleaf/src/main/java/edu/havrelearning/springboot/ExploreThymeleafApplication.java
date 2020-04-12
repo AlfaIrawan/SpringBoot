07a8e1a6afafa2ce085ee15fa4f8445acea3edce
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-13 05:54:02 +0700

    1.5.1 Additional

---
+++ explore-thymeleaf/src/main/java/edu/havrelearning/springboot/ExploreThymeleafApplication.java
@@ -0,0 +1,13 @@
+package edu.havrelearning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreThymeleafApplication.class, args);
+	}
+
+}
