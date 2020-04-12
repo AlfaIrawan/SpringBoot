861d4561d4eb02f034ea8634a95e6e396ec5e3c6
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-13 05:53:50 +0700

    1.5.0 Exploring thymeleaf

---
+++ explore-thymeleaf/src/main/java/edu/havrelearning/springboot/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package edu.havrelearning.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		
+		return "hello";
+		
+	}
+
+}
