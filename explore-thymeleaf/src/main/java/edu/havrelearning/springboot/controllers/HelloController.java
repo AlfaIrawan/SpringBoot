b31946657483cfc4f45fdf6b2ed92b69376ef00f
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-14 06:16:56 +0700

    1.5.5 Sending object data

--- explore-thymeleaf/src/main/java/edu/havrelearning/springboot/controllers/HelloController.java
+++ explore-thymeleaf/src/main/java/edu/havrelearning/springboot/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.havrelearning.springboot.models.Student;
+
 @Controller
 public class HelloController {
 	
@@ -19,6 +21,21 @@ public class HelloController {
 		
 		ModelAndView modelAndView = new ModelAndView("data");
 		modelAndView.addObject("message", "Take up one idea and make it your life");
+		return modelAndView;
+		
+	}
+	
+	@RequestMapping(value = "/student")
+	public ModelAndView getStudent() {
+		
+		ModelAndView modelAndView = new ModelAndView("student");
+		
+		Student student = new Student();
+		student.setName("Alfa");
+		student.setScore(100);
+		
+		modelAndView.addObject("student", student);
+		
 		return modelAndView;
 		
 	}
---
+++ explore-thymeleaf/src/main/java/edu/havrelearning/springboot/models/Student.java
@@ -0,0 +1,28 @@
+package edu.havrelearning.springboot.models;
+
+
+
+public class Student {
+
+	private String name;
+	private int score;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() { return name; }
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) { this.name = name; }
+	
+	/**
+	 * @return the score
+	 */
+	public int getScore() { return score; }
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) { this.score = score; }
+	
+}
