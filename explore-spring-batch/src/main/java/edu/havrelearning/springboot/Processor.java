5a21c2421f3251337409dc882f5b5287720b9120
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-15 07:59:39 +0700

    1.6.1 Implement writer, reader, and process

---
+++ explore-spring-batch/src/main/java/edu/havrelearning/springboot/Processor.java
@@ -0,0 +1,18 @@
+package edu.havrelearning.springboot;
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+
+
+public class Processor implements ItemProcessor<String, String> {
+
+	@Override
+	public String process(String item) throws Exception {
+		
+		System.out.println("Inside process");
+		return "PROCESSED " + item.toUpperCase();
+		
+	}
+	
+}
