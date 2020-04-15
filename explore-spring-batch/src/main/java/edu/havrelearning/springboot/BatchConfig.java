961e63fd3247e1f7114785f13227161c7ca78aff
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-15 08:09:34 +0700

    1.5.3 Configure the step

--- explore-spring-batch/src/main/java/edu/havrelearning/springboot/BatchConfig.java
+++ explore-spring-batch/src/main/java/edu/havrelearning/springboot/BatchConfig.java
@@ -1,6 +1,9 @@
 package edu.havrelearning.springboot;
 
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +11,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfig {
+	
+	@Autowired
+	private StepBuilderFactory sbf ;
 
+	@Bean
+	public Step Step() {
+		
+		return sbf.get("Step1")
+				.<String, String>chunk(1)
+				.reader(reader())
+				.writer(writer())
+				.processor(processor())
+				.build();
+		
+	}
+	
 	@Bean
 	public Reader reader() {
 		
