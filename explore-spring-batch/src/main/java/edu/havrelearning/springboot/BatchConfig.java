db5710c5672ed0ed467af902e35fa371efb5bbba
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-15 08:16:39 +0700

    1.6.4 Configure the job

--- explore-spring-batch/src/main/java/edu/havrelearning/springboot/BatchConfig.java
+++ explore-spring-batch/src/main/java/edu/havrelearning/springboot/BatchConfig.java
@@ -1,8 +1,11 @@
 package edu.havrelearning.springboot;
 
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,9 @@ public class BatchConfig {
 	
 	@Autowired
 	private StepBuilderFactory sbf ;
+	
+	@Autowired
+	private JobBuilderFactory jbf;
 
 	@Bean
 	public Step Step() {
@@ -23,6 +29,17 @@ public class BatchConfig {
 				.reader(reader())
 				.writer(writer())
 				.processor(processor())
+				.build();
+		
+	}
+	
+	@Bean
+	public Job Job() {
+		
+		return jbf.get("Job1")
+				.incrementer(new RunIdIncrementer())
+				.listener(myJobListener())
+				.start(Step())
 				.build();
 		
 	}
