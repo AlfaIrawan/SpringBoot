810e584ea4e6a4840f46e92a6ff105e361c7b435
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-16 11:26:36 +0700

    1.7.7 Write a test

--- batch-cvs-to-db/src/main/java/edu/havrelearning/springboot/BatchCvsToDbApplication.java
+++ batch-cvs-to-db/src/main/java/edu/havrelearning/springboot/BatchCvsToDbApplication.java
@@ -1,9 +1,11 @@
 package edu.havrelearning.springboot;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchCvsToDbApplication {
 
 	public static void main(String[] args) {
